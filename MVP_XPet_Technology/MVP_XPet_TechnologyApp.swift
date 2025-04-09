e09fc605119b7e7487d3368e04f6467c7ef0034b
Nathan Zhang <nml707805@gmail.com>
2025-04-08 23:34:21 -0700

    Initial Commit

---
+++ MVP_XPet_Technology/MVP_XPet_TechnologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVP_XPet_TechnologyApp.swift
+//  MVP_XPet_Technology
+//
+//  Created by Nathan Zhang on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MVP_XPet_TechnologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
